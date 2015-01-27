181222a98e3d3a417e41c1599d99e3a6b9042499
Brian King <brian.king@raizlabs.com>
2015-01-27 01:49:33 -0500

    Initial Commit

---
+++ RZAssemblage/RZAssemblage.h
@@ -0,0 +1,19 @@
+//
+//  RZAssemblage.h
+//  RZAssemblage
+//
+//  Created by Brian King on 1/27/15.
+//  Copyright (c) 2015 Raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RZAssemblage.
+FOUNDATION_EXPORT double RZAssemblageVersionNumber;
+
+//! Project version string for RZAssemblage.
+FOUNDATION_EXPORT const unsigned char RZAssemblageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RZAssemblage/PublicHeader.h>
+
+
